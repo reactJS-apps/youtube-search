e23dd94406d8ba742948e7b0908ed4f049c86e03
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-27 21:20:42 +0500

    display selected youtube video in the iframe tag

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const VideoDetail = ({ selectedVideo }) => {
-  const { title, description } = selectedVideo.snippet;
   if (!selectedVideo) {
     return <div>Loading...</div>;
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${selectedVideo.id.videoId}`;
+  const { title, description } = selectedVideo.snippet;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc}></iframe>
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{title}</h4>
         <p>{description}</p>
