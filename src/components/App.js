c16341b2e8e83fc650b2653ea238b27acd2c3f9e
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-27 16:53:53 +0500

    fetch videos from youtube data api and update state with fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import { Component } from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends Component {
+  state = {
+    videos: [],
+  };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+    console.log(term);
+    console.log(response);
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,7 @@ class SearchBar extends Component {
 
   onFormSubmit = (e) => {
     e.preventDefault();
+    this.props.onFormSubmit(this.state.searchTerm);
   };
 
   render() {
